153cdc442511f7e2301cc40a000001c005da84d0
Leandro Liduvino <leandro.augusto460@gmail.com>
2021-08-02 14:58:09 -0300

    cumpre requisito 1

--- src/App.js
+++ src/App.js
@@ -1,8 +1,20 @@
 import React from 'react';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import { MovieList, MovieDetails, EditMovie, NewMovie, NotFound } from './pages';
 
 function App() {
   return (
-    <div>Movie Card Library CRUD</div>
+    <BrowserRouter>
+      {/* <div>Movie Card Library CRUD</div> */}
+      <Switch>
+        <Route path="/" exact component={ MovieList } />
+        <Route path="/movies/new" exact component={ NewMovie } />
+        <Route path="/movies/:id" exact component={ MovieDetails } />
+        <Route path="/movies/:id/edit" exact component={ EditMovie } />
+        <Route path="/:any" component={ NotFound } />
+
+      </Switch>
+    </BrowserRouter>
   );
 }
 
