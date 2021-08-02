615b07253e6128242c7b863cf02e37a92d46d9d9
ElaineMoreira <seuemail@exemplo.br>
2021-08-02 18:36:49 -0300

    Adiciona requisito 3

--- src/components/MovieCard.js
+++ src/components/MovieCard.js
@@ -1,13 +1,35 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import PropTypes from 'prop-types';
+// consulta - Um objeto assumindo uma forma particular - documentation "shape"
+// optionalObjectWithShape: PropTypes.shape({
+// color: PropTypes.string,
+// fontSize: PropTypes.number
+// }),
 
 class MovieCard extends React.Component {
   render() {
+    const { movie } = this.props;
+    const { title, imagePath, storyline, id } = movie;
+
     return (
       <div data-testid="movie-card">
-        Movie Card
+        <h3>{ title }</h3>
+        <img src={ imagePath } alt={ title } />
+        <p>{ storyline }</p>
+        <Link to={ `/movies/${id}` }> VER DETALHES </Link>
       </div>
     );
   }
 }
+
+MovieCard.propTypes = {
+  movie: PropTypes.shape({
+    title: PropTypes.string,
+    imagePath: PropTypes.string,
+    storyline: PropTypes.string,
+    id: PropTypes.number,
+  }).isRequired,
+};
 
 export default MovieCard;
