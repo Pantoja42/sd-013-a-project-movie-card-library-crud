dab342ff5c17ac828555aea62c4e4ac17c12ca7f
vitorgonzaga <vitor.gonzagaf1@gmail.com>
2021-08-02 18:13:32 -0300

    Incluindo a versão fetch e passando props para MovieCard via map no MovieList

--- src/pages/MovieList.js
+++ src/pages/MovieList.js
@@ -1,25 +1,49 @@
 import React, { Component } from 'react';
 import MovieCard from '../components/MovieCard';
-
+import Loading from '../components/Loading';
 import * as movieAPI from '../services/movieAPI';
 
 class MovieList extends Component {
   constructor() {
     super();
 
+    this.fetchMovies = this.fetchMovies.bind(this);
+
     this.state = {
+      loading: true,
       movies: [],
     };
   }
 
+  // https://www.valentinog.com/blog/await-react/
+  // componentDidMount() {
+  //   movieAPI.getMovies().then((res) => this.setState({ movies: res }));
+  // }
+
+  componentDidMount() {
+    this.fetchMovies();
+  }
+
+  async fetchMovies() {
+    this.setState(
+      { loading: true },
+      async () => {
+        const requestedMovies = await movieAPI.getMovies();
+        console.log(requestedMovies);
+        this.setState({
+          movies: [...requestedMovies],
+          loading: false,
+        });
+      },
+    );
+  }
+
   render() {
-    const { movies } = this.state;
-
-    // Render Loading here if the request is still happening
-
+    const { movies, loading } = this.state;
     return (
       <div data-testid="movie-list">
-        {movies.map((movie) => <MovieCard key={ movie.title } movie={ movie } />)}
+        {loading ? <Loading />
+          : movies.map((movie) => <MovieCard key={ movie.title } movie={ movie } />)}
       </div>
     );
   }
