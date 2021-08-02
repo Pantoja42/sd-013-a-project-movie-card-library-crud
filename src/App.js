d24671f735b5adb3c48840f96d09281c7f8a1188
Gustavo Mauricio <gmauricio207@gmail.com>
2021-08-02 14:50:38 -0300

    Req 01

--- src/App.js
+++ src/App.js
@@ -1,9 +1,20 @@
-import React from 'react';
+import React, { Component } from 'react';
+import { BrowserRouter } from 'react-router-dom';
+import { EditMovie, MovieDetails, MovieList, NotFound, NewMovie } from './pages/index';
 
-function App() {
-  return (
-    <div>Movie Card Library CRUD</div>
-  );
+export default class App extends Component {
+  render() {
+    return (
+      <BrowserRouter>
+        <div>
+          <div>Movie Card Library CRUD</div>
+          <MovieList />
+          <MovieDetails />
+          <NewMovie />
+          <EditMovie />
+          <NotFound />
+        </div>
+      </BrowserRouter>
+    );
+  }
 }
-
-export default App;
