0b2f96207669ecc0148f06983ef7fb81e6a64d9e
Felipe Lima <felipe_lima10@icloud.com>
2021-08-02 15:09:00 -0300

    Renderize BrowserRouter no componente App usando rotas

--- src/App.js
+++ src/App.js
@@ -1,12 +1,20 @@
 import React from 'react';
-import { BrowserRouter as Router } from 'react-router-dom';
+import { BrowserRouter, Route } from 'react-router-dom';
+// https://github.com/tryber/sd-13a-live-lectures/pull/40/files?file-filters%5B%5D=.ico&file-filters%5B%5D=.js&file-filters%5B%5D=.png
+import { EditMovie, MovieDetails, MovieList, NewMovie, NotFound } from './pages/index';
 import './App.css';
 
+// https://reactrouter.com/web/example/no-match usei no NotFound
 function App() {
   return (
-    <Router>
-      <div>Movie Card Library</div>
-    </Router>
+    <BrowserRouter>
+      <Route exact path="/" component={ MovieList } />
+      <Route path="/movies/:id" component={ MovieDetails } />
+      <Route path="/movies/new" component={ NewMovie } />
+      <Route path="/movies/:id/edit" component={ EditMovie } />
+      <Route path="*"><NotFound /></Route>
+    </BrowserRouter>
+
   );
 }
 
