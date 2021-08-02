b43dc7c316f6db4b6db05d41c54017f2b02c9785
Samuel Guilherme <samuel.guilherme.l@hotmail.com>
2021-08-02 18:01:03 -0300

    resolvendo requisito 2 e lint

--- src/pages/MovieList.js
+++ src/pages/MovieList.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-// import { Loading } from '../components';
+import { Loading } from '../components';
 import MovieCard from '../components/MovieCard';
 
 import * as movieAPI from '../services/movieAPI';
@@ -13,14 +13,18 @@ class MovieList extends Component {
   }
 
   componentDidMount() {
-    movieAPI.getMovies();
+    movieAPI.getMovies()
+      .then((data) => {
+        this.setState({
+          movies: data,
+        });
+      });
   }
 
   render() {
     const { movies } = this.state;
 
-    //  if (movies.length === 0) return <Loading />;
-
+    if (movies.length === 0) return <Loading />;
     return (
       <div data-testid="movie-list">
         {movies.map((movie) => <MovieCard key={ movie.title } movie={ movie } />)}
