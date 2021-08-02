798b50f047b6396e7f1dfaf09da3b0ffde4f268c
EmmanuelBMS <acquabriall2020@gmail.com>
2021-08-02 18:03:29 -0300

    resolve req 3 e comenta linhas em MovieDetails EditMovie MovieForm para aprovar lint

--- src/components/MovieCard.js
+++ src/components/MovieCard.js
@@ -1,8 +1,10 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import PropTypes from 'prop-types';
 
 class MovieCard extends React.Component {
   render() {
-    const { movie: { title, storyline, imagePath } } = this.props;
+    const { movie: { title, storyline, imagePath, id } } = this.props;
     return (
       <div data-testid="movie-card">
         <img alt="Movie Cover" src={ imagePath } />
@@ -10,9 +12,19 @@ class MovieCard extends React.Component {
         <div>
           <p>{ storyline }</p>
         </div>
+        <Link to={ `movies/${id}` }>VER DETALHES</Link>
       </div>
     );
   }
 }
+
+MovieCard.propTypes = {
+  movie: PropTypes.shape({
+    title: PropTypes.string,
+    storyline: PropTypes.string,
+    imagePath: PropTypes.string,
+    id: PropTypes.number,
+  }).isRequired,
+};
 
 export default MovieCard;
