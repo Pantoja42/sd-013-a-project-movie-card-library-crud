df6340bed8dad0d08be5acd303af34b3a12ece70
Gustavo-Jezini <gustavojezini@gmail.com>
2021-08-02 14:24:11 -0300

    commit inicial

--- src/App.js
+++ src/App.js
@@ -1,8 +1,14 @@
 import React from 'react';
+import { BrowserRouter, Route } from 'react-router-dom';
+import './App.css';
 
 function App() {
   return (
-    <div>Movie Card Library CRUD</div>
+    <BrowserRouter>
+      <Route>
+        <div>Movie Card Library CRUD</div>
+      </Route>
+    </BrowserRouter>
   );
 }
 
