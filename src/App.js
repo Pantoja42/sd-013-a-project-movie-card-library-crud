cca155fe20ef69c3497a0bf31607a97c4552fb7e
Tatiane Chiarello <tatichiarello@gmail.com>
2021-08-03 00:40:06 +0200

    cria rotas no app

--- src/App.js
+++ src/App.js
@@ -1,11 +1,25 @@
 import React from 'react';
-import { BrowserRouter } from 'react-router-dom';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
 import './App.css';
+import {
+  MovieList,
+  MovieDetails,
+  NewMovie,
+  EditMovie,
+  NotFound,
+} from './pages';
 
 function App() {
   return (
     <BrowserRouter>
       <div>Movie Card Library CRUD</div>
+      <Switch>
+        <Route exact path='/' component={MovieList}></Route>
+        <Route exact path='/movies/new' component={NewMovie}></Route>
+        <Route exact path='/movies/:id' component={MovieDetails}></Route>
+        <Route exact path='/movies/:id/edit' component={EditMovie}></Route>
+        <Route path='*' component={NotFound}></Route>
+      </Switch>
     </BrowserRouter>
   );
 }
