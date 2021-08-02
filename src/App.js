92d0ef9123ac823ad51a80ca93e19ee726d2c615
Gabriel Biasoli <bielbiasoli33@icloud.com>
2021-08-02 14:58:25 -0300

    Finishes requirement 1

--- src/App.js
+++ src/App.js
@@ -1,8 +1,22 @@
 import React from 'react';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
+import { MovieList, NewMovie, EditMovie, NotFound, MovieDetails } from './pages';
 
 function App() {
   return (
-    <div>Movie Card Library CRUD</div>
+    <BrowserRouter>
+      <Switch>
+        <Route exact path="/" component={ MovieList } />
+        <Route path="/movies/new" component={ NewMovie } />
+        <Route exact path="/movies/:id" render={ (props) => <MovieDetails { ...props } /> } />
+        <Route
+          path="/movies/:id/edit"
+          component={ (props) => <EditMovie { ...props } /> }
+        />
+        <Route path="*" component={ NotFound } />
+      </Switch>
+      <div>Movie Card Library CRUD</div>
+    </BrowserRouter>
   );
 }
 
