b0acd69d78c23ec1dc888e5bfdc335a15ad69b22
EmmanuelBMS <acquabriall2020@gmail.com>
2021-08-02 17:48:49 -0300

    resolve requisito 2 sem lint

--- src/components/MovieCard.js
+++ src/components/MovieCard.js
@@ -2,9 +2,14 @@ import React from 'react';
 
 class MovieCard extends React.Component {
   render() {
+    const { movie: { title, storyline, imagePath } } = this.props;
     return (
       <div data-testid="movie-card">
-        Movie Card
+        <img alt="Movie Cover" src={ imagePath } />
+        <h3>{ title }</h3>
+        <div>
+          <p>{ storyline }</p>
+        </div>
       </div>
     );
   }
