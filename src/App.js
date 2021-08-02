a146af55ba7316fe4dd5a24c86f54547fcdb4331
Felipe Lima <felipe_lima10@icloud.com>
2021-08-02 18:07:45 -0300

    resolve erros de lint

--- src/App.js
+++ src/App.js
@@ -9,11 +9,29 @@ function App() {
   return (
     <BrowserRouter>
       <Switch>
-        <Route exact path="/" component={ MovieList } />
-        <Route exact path="/movies/:id" render={ (props) => <MovieDetails { ...props } /> } />
-        <Route exact path="/movies/new" component={ NewMovie } />
-        <Route exact path="/movies/:id/edit" render={ (props) => <EditMovie { ...props } /> } />
-        <Route path="*"><NotFound /></Route>
+        <Route
+          exact
+          path="/"
+          component={ MovieList }
+        />
+        <Route
+          exact
+          path="/movies/:id"
+          render={ (props) => <MovieDetails { ...props } /> }
+        />
+        <Route
+          exact
+          path="/movies/new"
+          component={ NewMovie }
+        />
+        <Route
+          exact
+          path="/movies/:id/edit"
+          render={ (props) => <EditMovie { ...props } /> }
+        />
+        <Route path="*">
+          <NotFound />
+        </Route>
       </Switch>
     </BrowserRouter>
 
