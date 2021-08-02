f331b5fd5398681f2cb709402cbdf6016841ad48
V. Dionysio <vini.dionysio@gmail.com>
2021-08-02 14:59:05 -0300

    Req 1

--- src/App.js
+++ src/App.js
@@ -1,8 +1,16 @@
 import React from 'react';
+import { BrowserRouter, Route } from 'react-router-dom';
+import { MovieList, MovieDetails, NewMovie, EditMovie, NotFound } from './pages/index';
 
 function App() {
   return (
-    <div>Movie Card Library CRUD</div>
+    <BrowserRouter>
+      <Route path="/" component={ MovieList } />
+      <Route path="/movies/:id" component={ MovieDetails } />
+      <Route path="/movies/new" component={ NewMovie } />
+      <Route path="/movies/:id/edit" component={ EditMovie } />
+      <Route path="/:notfound" component={ NotFound } />
+    </BrowserRouter>
   );
 }
 
