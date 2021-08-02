6051ea0d516170e9b5175ec932fa374cd21cd9a1
Leonardo Santos <lcds90@gmail.com>
2021-08-02 16:55:55 -0300

    req 1...

--- src/App.js
+++ src/App.js
@@ -1,11 +1,34 @@
 import React from 'react';
-import { BrowserRouter as Router } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import './App.css';
+import { Navbar } from './components';
+import {
+  EditMovie,
+  MovieDetails,
+  MovieList,
+  NewMovie,
+  NotFound,
+} from './pages';
 
 function App() {
+  const linksInfo = [
+    { url: '/', component: <MovieList /> },
+    { url: '/movies/:id', component: <MovieDetails /> },
+    { url: '/movies/new', component: <NewMovie /> },
+    { url: '/movies/:id/edit', component: <EditMovie /> },
+    { url: '*', component: <NotFound /> },
+  ];
   return (
     <Router>
       <div>Movie Card Library CRUD</div>
+      <Navbar links={ linksInfo } />
+      <Switch>
+        {linksInfo.map((link) => (
+          <Route key={ link.url } exact path={ link.url }>
+            {link.component}
+          </Route>
+        ))}
+      </Switch>
     </Router>
   );
 }
---
+++ src/components/Navbar.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import PropTypes from 'prop-types';
+
+class Navbar extends React.Component {
+  render() {
+    const { links } = this.props;
+    return (
+      links.map((link) => (
+        <Link
+          key={ link.url }
+          to={ link.url }
+          Component={ link.component }
+        />
+      ))
+    );
+  }
+}
+
+Navbar.propTypes = {
+  links: PropTypes.arrayOf(PropTypes.object).isRequired,
+};
+
+export default Navbar;
