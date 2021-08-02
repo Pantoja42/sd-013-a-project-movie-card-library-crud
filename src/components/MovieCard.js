6185e20ef3f2025f457f657af591231630e0f237
vitorgonzaga <vitor.gonzagaf1@gmail.com>
2021-08-02 18:14:27 -0300

    Tratando as props recebidas no MovieCard e editando o componente Loading

--- src/components/MovieCard.js
+++ src/components/MovieCard.js
@@ -1,13 +1,21 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
 class MovieCard extends React.Component {
   render() {
+    const { movie: { title } } = this.props;
     return (
       <div data-testid="movie-card">
-        Movie Card
+        <h2>{ title }</h2>
       </div>
     );
   }
 }
+
+MovieCard.propTypes = {
+  movie: PropTypes.shape({
+    title: PropTypes.string,
+  }).isRequired,
+};
 
 export default MovieCard;
