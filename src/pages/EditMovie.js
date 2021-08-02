15d78c0de5f2ab43da8231398cbeef4e58b21822
Victor Diniz <victoralbertocostadiniz@gmail.com>
2021-08-02 17:18:01 -0300

    Completa o quinto requisito

--- src/pages/EditMovie.js
+++ src/pages/EditMovie.js
@@ -1,26 +1,45 @@
 import React, { Component } from 'react';
-
-import { MovieForm } from '../components';
+import { Redirect } from 'react-router-dom';
+import PropTypes from 'prop-types';
+import { Loading, MovieForm } from '../components';
 import * as movieAPI from '../services/movieAPI';
 
 class EditMovie extends Component {
   constructor(props) {
     super(props);
-    this.state = {};
+    this.state = {
+      movie: {},
+      loading: true,
+      shouldRedirect: false,
+    };
     this.handleSubmit = this.handleSubmit.bind(this);
   }
 
+  // Aqui peguei a mesma função do arquivo MovieDetails, com a intenção de justamente salvar o filme correto no estado e passar isso para MovieForm como uma prop.
+  componentDidMount() {
+    const { match } = this.props;
+    const { id } = match.params;
+    movieAPI.getMovie(id)
+      .then((r) => this.setState({
+        movie: r,
+        loading: false,
+      }));
+  }
+
   handleSubmit(updatedMovie) {
+    movieAPI.updateMovie(updatedMovie).then(() => this.setState({
+      shouldRedirect: true,
+    }));
   }
 
   render() {
-    const { status, shouldRedirect, movie } = this.state;
+    const { loading, shouldRedirect, movie } = this.state;
     if (shouldRedirect) {
-      // Redirect
+      return <Redirect to="/" />;
     }
 
-    if (status === 'loading') {
-      // render Loading
+    if (loading === true) {
+      return <Loading />;
     }
 
     return (
@@ -30,5 +49,13 @@ class EditMovie extends Component {
     );
   }
 }
+
+EditMovie.propTypes = {
+  match: PropTypes.shape({
+    params: PropTypes.shape({
+      id: PropTypes.number,
+    }).isRequired,
+  }).isRequired,
+};
 
 export default EditMovie;
