4268b079b0895030c05e84955d6deda048d0b91f
Rogerio P. Da Silva <rogeriop1990cv@gmail.com>
2021-08-03 01:26:06 -0300

    Requisito 3 OK

--- src/components/MovieCard.js
+++ src/components/MovieCard.js
@@ -1,10 +1,15 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 class MovieCard extends React.Component {
   render() {
+    const { movie: { title, storyline, id } } = this.props;
+    console.log(title);
     return (
       <div data-testid="movie-card">
-        Movie Card
+        <h1>{title}</h1>
+        <p>{storyline}</p>
+        <p><Link to={ `movies/${id}` }>VER DETALHES</Link></p>
       </div>
     );
   }
