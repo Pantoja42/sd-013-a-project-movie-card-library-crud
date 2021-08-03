96421c8dc16a97f49dbcad471f9f6b8c808a7df4
Adriana Barreto <adri92barreto123@gmail.com>
2021-08-03 19:20:34 -0300

    Commit inicial

--- src/App.js
+++ src/App.js
@@ -1,8 +1,22 @@
+// import React from 'react';
+
+// function App() {
+//   return (
+//     <div>Movie Card Library CRUD</div>
+//   );
+// }
+
+// export default App;
+
 import React from 'react';
+import { BrowserRouter as Router } from 'react-router-dom';
+import './App.css';
 
 function App() {
   return (
-    <div>Movie Card Library CRUD</div>
+    <Router>
+      <div>Movie Card Library CRUD</div>
+    </Router>
   );
 }
 
