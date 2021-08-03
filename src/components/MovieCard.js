0f283d2e56dbdc81c72091293bb3588236c74f98
thomasravache <thomasravache31@gmail.com>
2021-08-03 15:14:02 -0300

    2 - Faça uma requisição para buscar e mostrar a lista de filmes quando MovieList for montado

--- src/components/MovieCard.js
+++ src/components/MovieCard.js
@@ -2,9 +2,32 @@ import React from 'react';
 
 class MovieCard extends React.Component {
   render() {
+    const {
+      movie: {
+        id,
+        title,
+        subtitle,
+        storyline,
+        rating,
+        imagePath,
+        bookmarked,
+        genre,
+      }
+    } = this.props;
+    
     return (
       <div data-testid="movie-card">
-        Movie Card
+        <section>
+          <img src={ imagePath } alt={ `Filme: ${ title }` } />
+          <h1>{ title }</h1>
+        </section>
+        <section>
+          <p>{ storyline }</p>
+          <hr />
+        </section>
+        <section>
+          <button>Ver Detalhes</button>
+        </section>
       </div>
     );
   }
--- src/pages/MovieList.js
+++ src/pages/MovieList.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import MovieCard from '../components/MovieCard';
+import { MovieCard ,Loading } from '../components';
 
 import * as movieAPI from '../services/movieAPI';
 
@@ -10,16 +10,32 @@ class MovieList extends Component {
     this.state = {
       movies: [],
     };
+    
+    this.fetchMovie = this.fetchMovie.bind(this);
+  }
+
+  fetchMovie() {
+    movieAPI.getMovies()
+      .then((data) => (
+        this.setState({
+          movies: data,
+        })
+      ))
+  }
+
+  componentDidMount() {
+    this.fetchMovie();
   }
 
   render() {
     const { movies } = this.state;
+    const isEmptyMovies = (movies.length === 0);
 
     // Render Loading here if the request is still happening
 
     return (
       <div data-testid="movie-list">
-        {movies.map((movie) => <MovieCard key={ movie.title } movie={ movie } />)}
+        { isEmptyMovies ? <Loading /> : movies.map((movie) => <MovieCard key={ movie.title } movie={ movie } />) }
       </div>
     );
   }
