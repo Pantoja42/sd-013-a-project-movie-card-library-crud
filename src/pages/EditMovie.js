122db541290d7c82c4ab2e11610182d8f804b347
thomasravache <thomasravache31@gmail.com>
2021-08-03 20:15:02 -0300

    5 - Realize uma requisição para buscar o filme que será editado em EditMovie

--- src/pages/EditMovie.js
+++ src/pages/EditMovie.js
@@ -1,26 +1,52 @@
 import React, { Component } from 'react';
-
-import { MovieForm } from '../components';
+import { MovieForm, Loading } from '../components';
 import * as movieAPI from '../services/movieAPI';
+import { Redirect } from 'react-router-dom';
 
 class EditMovie extends Component {
   constructor(props) {
     super(props);
-    this.state = {};
+    this.state = {
+      movie: {},
+      shouldRedirect: false,
+      status: 'loading',
+    };
     this.handleSubmit = this.handleSubmit.bind(this);
+    this.fetchMovie = this.fetchMovie.bind(this);
   }
 
-  handleSubmit(updatedMovie) {
+  async fetchMovie() {
+    const { id } = this.props.match.params;
+    const movie = await movieAPI.getMovie(id);
+
+    this.setState({
+      movie,
+      status: 'loaded',
+    })
+  }
+
+  componentDidMount() {
+    this.fetchMovie();
+  }
+
+  async handleSubmit(updatedMovie) {
+    const update = await movieAPI.updateMovie(updatedMovie);
+
+    this.setState({
+      shouldRedirect: true,
+    })
+
+    return update;
   }
 
   render() {
     const { status, shouldRedirect, movie } = this.state;
     if (shouldRedirect) {
-      // Redirect
+      return <Redirect to="/" />
     }
 
     if (status === 'loading') {
-      // render Loading
+      return <Loading />
     }
 
     return (
