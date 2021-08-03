08e72e60061cad202efd61b43767bcb625dcf80c
Juliane Marques <mqs.juliane@gmail.com>
2021-08-03 06:02:48 -0300

    Ajustes no requisito 5

--- src/pages/EditMovie.js
+++ src/pages/EditMovie.js
@@ -3,6 +3,7 @@ import PropTypes from 'prop-types';
 
 import { Loading, MovieForm } from '../components';
 import * as movieAPI from '../services/movieAPI';
+import { Redirect } from 'react-router';
 
 class EditMovie extends Component {
   constructor(props) {
@@ -21,7 +22,7 @@ class EditMovie extends Component {
   }
 
   async editMovie() {
-    const {  match } = this.props;
+    const { match } = this.props;
     const { id } = match.params;
     const movie = await movieAPI.getMovie(id);
     this.setState({
@@ -43,7 +44,7 @@ class EditMovie extends Component {
   render() {
     const { status, shouldRedirect, movie } = this.state;
     if (shouldRedirect) {
-      // Redirect
+      return <Redirect to="/" />;
     }
 
     if (status === 'loading') {
