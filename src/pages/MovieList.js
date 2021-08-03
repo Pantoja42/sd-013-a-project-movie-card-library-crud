2275f125014e055aa7fc32daabf78dd931965926
Tomas Santos <tomas.slvsts@gmail.com>
2021-08-03 15:24:18 -0300

    corrigindo questões e lint

--- src/pages/MovieList.js
+++ src/pages/MovieList.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
 import MovieCard from '../components/MovieCard';
+import Loading from '../components/Loading';
 
 import * as movieAPI from '../services/movieAPI';
 
@@ -9,17 +11,34 @@ class MovieList extends Component {
 
     this.state = {
       movies: [],
+      loading: true,
     };
+
+    this.changeState = this.changeState.bind(this);
+  }
+
+  componentDidMount() {
+    this.changeState();
+  }
+
+  async changeState() {
+    const moviesList = await movieAPI.getMovies();
+    this.setState({
+      movies: moviesList,
+      loading: false,
+    });
   }
 
   render() {
-    const { movies } = this.state;
+    const { movies, loading } = this.state;
 
     // Render Loading here if the request is still happening
 
     return (
       <div data-testid="movie-list">
-        {movies.map((movie) => <MovieCard key={ movie.title } movie={ movie } />)}
+        {loading ? <Loading /> : movies.map((movie) => (
+          <MovieCard key={ movie.title } movie={ movie } />))}
+        <Link to="/movies/new">ADICIONAR CARTÃO</Link>
       </div>
     );
   }
