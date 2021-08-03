aed882b7ec9fa86a4688e330bf05a298534a770d
icaroMelo <exemployt@gmail.com>
2021-08-03 11:42:29 -0300

    Requisito 2

--- src/pages/MovieList.js
+++ src/pages/MovieList.js
@@ -1,7 +1,8 @@
 import React, { Component } from 'react';
 import MovieCard from '../components/MovieCard';
-
+import { Loading } from '../components';
 import * as movieAPI from '../services/movieAPI';
+// codigo feito com ajuda de estudantes em uma sala de estudos
 
 class MovieList extends Component {
   constructor() {
@@ -9,13 +10,32 @@ class MovieList extends Component {
 
     this.state = {
       movies: [],
+      load: false,
     };
+  }
+  
+  // Prciso definir o movies como a api, por isso crio uma função fora, pois se criar uma unica não terá como chamar dentro dela mesmo, além de não poder usar o this.
+
+  fetchAPI = async () => {
+    const response = await movieAPI.getMovies();
+    this.setState(() => ({
+      movies: response,
+      load: true,
+    }));
+
+  }
+
+  componentDidMount() {
+   this.fetchAPI();
   }
 
   render() {
-    const { movies } = this.state;
+    const { movies , load} = this.state;
+   // Render Loading here if the request is still happening (dica de ouro)
 
-    // Render Loading here if the request is still happening
+    if(load === false){
+      return <Loading />;
+    }
 
     return (
       <div data-testid="movie-list">
