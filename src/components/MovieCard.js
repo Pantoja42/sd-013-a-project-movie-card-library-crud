99e828c42e26601866c21464d7309db7078eb601
sonkai <mfilho86@gmail.com>
2021-08-03 18:31:35 -0300

    Req 3 ver detalhes

--- src/components/MovieCard.js
+++ src/components/MovieCard.js
@@ -1,16 +1,18 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import { Link } from 'react-router-dom';
 
 class MovieCard extends React.Component {
   render() {
     const { movie } = this.props;
-    const { title, subtitle, storyline, imagePath } = movie;
+    const { title, subtitle, storyline, imagePath, id } = movie;
     return (
       <div data-testid="movie-card">
         <h1 data-testid="movie-card-title">{ title }</h1>
         <h2>{ subtitle }</h2>
         <img src={ imagePath } alt="Capa do Filme" />
         <p>{ storyline }</p>
+        <Link to={ `/movies/${id}` }>VER DETALHES</Link>
       </div>
     );
   }
@@ -22,6 +24,7 @@ MovieCard.propTypes = {
     subtitle: PropTypes.string,
     storyline: PropTypes.string,
     imagePath: PropTypes.string,
+    id: PropTypes.number,
   }).isRequired,
 };
 
