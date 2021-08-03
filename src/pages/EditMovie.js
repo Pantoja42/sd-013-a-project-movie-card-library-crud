7927f7d1d8fc111354123001b30b0d88e7c298c9
Juliane Marques <mqs.juliane@gmail.com>
2021-08-03 19:16:02 -0300

    arruma erros 3

--- src/pages/EditMovie.js
+++ src/pages/EditMovie.js
@@ -1,9 +1,9 @@
-import React, { Component } from "react";
-import { Redirect } from "react-router-dom";
-import PropTypes from "prop-types";
-import { Loading, MovieForm } from "../components";
+import React, { Component } from 'react';
+import { Redirect } from 'react-router-dom';
+import PropTypes from 'prop-types';
+import { Loading, MovieForm } from '../components';
 
-import * as movieAPI from "../services/movieAPI";
+import * as movieAPI from '../services/movieAPI';
 
 class EditMovie extends Component {
   constructor(props) {
@@ -58,7 +58,7 @@ class EditMovie extends Component {
 
     return (
       <div data-testid="edit-movie">
-        <MovieForm movie={movie} onSubmit={this.handleSubmit} />
+        <MovieForm movie={ movie } onSubmit={ this.handleSubmit } />
       </div>
     );
   }
--- src/pages/MovieList.js
+++ src/pages/MovieList.js
@@ -44,7 +44,7 @@ class MovieList extends Component {
         {movies.map((movie) => (
           <MovieCard key={ movie.title } movie={ movie } />
         ))}
-        <Link to='/movies/new'>ADICIONAR CARTÃO</Link>
+        <Link to="/movies/new">ADICIONAR CARTÃO</Link>
       </div>
     );
   }
