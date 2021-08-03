52243e81cf16cea7c567bb0306dd4282f1eb0909
Juliane Marques <mqs.juliane@gmail.com>
2021-08-03 10:58:44 -0300

    Arruma EsLint tentativa 7

--- src/pages/MovieDetails.js
+++ src/pages/MovieDetails.js
@@ -39,7 +39,7 @@ class MovieDetails extends Component {
 
     return (
       <div data-testid="movie-details">
-        <img alt="Movie Cover" src={`../${imagePath }`}/>
+        <img alt="Movie Cover" src={ `../${imagePath}` } />
         <p>{`Title: ${title}`}</p>
         <p>{`Subtitle: ${subtitle}`}</p>
         <p>{`Storyline: ${storyline}`}</p>
