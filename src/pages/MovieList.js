78d90f5f689f5d60494de1550d50291570e2eabd
Matheus Duarte de Freitas <freickup@gmail.com>
2021-08-02 22:19:41 -0300

    resolvendo requisito do MovieCard

--- src/pages/MovieList.js
+++ src/pages/MovieList.js
@@ -1,25 +1,41 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
 import MovieCard from '../components/MovieCard';
-
 import * as movieAPI from '../services/movieAPI';
+import Loading from '../components/Loading';
 
 class MovieList extends Component {
   constructor() {
     super();
 
     this.state = {
+      loading: true,
       movies: [],
     };
+
+    this.resolveGetMovies = this.resolveGetMovies.bind(this);
+  }
+
+  componentDidMount() {
+    this.resolveGetMovies();
+  }
+
+  async resolveGetMovies() {
+    const filmes = await movieAPI.getMovies();
+    this.setState({
+      loading: false,
+      movies: filmes,
+    });
   }
 
   render() {
-    const { movies } = this.state;
-
-    // Render Loading here if the request is still happening
+    const { movies, loading } = this.state;
+    const addCard = <Link to="/movies/new">ADICIONAR CARTÃO</Link>
 
     return (
       <div data-testid="movie-list">
-        {movies.map((movie) => <MovieCard key={ movie.title } movie={ movie } />)}
+        {loading ? <Loading /> : addCard}
+        {movies.map((movie) => <MovieCard key={ movie.id } movie={ movie } />)}
       </div>
     );
   }
