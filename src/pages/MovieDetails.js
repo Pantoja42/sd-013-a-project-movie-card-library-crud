2cc4f117da55333c8c90cd8fe60b3039fe0d458a
thomasravache <thomasravache31@gmail.com>
2021-08-03 17:14:23 -0300

    4 - Faça uma requisição para buscar o filme que deverá ser renderizado dentro de Movie Details

--- src/pages/MovieDetails.js
+++ src/pages/MovieDetails.js
@@ -2,22 +2,58 @@ import React, { Component } from 'react';
 
 import * as movieAPI from '../services/movieAPI';
 import { Loading } from '../components';
+import { Link } from 'react-router-dom';
 
 class MovieDetails extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      movie: false,
+    }
+
+    this.fetchMovie = this.fetchMovie.bind(this);
+  }
+
+  async fetchMovie() {
+    const { id } = this.props.match.params;
+
+    const movie = await movieAPI.getMovie(id);
+
+    this.setState({
+      movie,
+    })
+  }
+
+  componentDidMount() {
+    this.fetchMovie();
+  }
+
   render() {
     // Change the condition to check the state
     // if (true) return <Loading />;
 
-    const { title, storyline, imagePath, genre, rating, subtitle } = {};
-
-    return (
+    const { id } = this.props.match.params;
+    const { movie } = this.state;
+    const { title, storyline, imagePath, genre, rating, subtitle } = movie;
+    const movieDetails = (
       <div data-testid="movie-details">
         <img alt="Movie Cover" src={ `../${imagePath}` } />
+        <p>{ `Title: ${title}` }</p>
         <p>{ `Subtitle: ${subtitle}` }</p>
         <p>{ `Storyline: ${storyline}` }</p>
         <p>{ `Genre: ${genre}` }</p>
         <p>{ `Rating: ${rating}` }</p>
+        <hr />
+        <div>
+          <Link to={ `/movies/${id}/edit` } >EDITAR</Link>
+          <Link to={ `/` } >VOLTAR</Link>
+        </div>
       </div>
+    );
+
+    return (
+      movie !== false ? movieDetails : <Loading />
     );
   }
 }
