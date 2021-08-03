140bb62d2402c642469ede1e883671dc48898837
Pedro Trasfereti <pedrotrasfereti@gmail.com>
2021-08-03 13:23:12 -0300

    requisito 3 completo

--- src/components/MovieCard.js
+++ src/components/MovieCard.js
@@ -1,13 +1,45 @@
 import React from 'react';
 
+import { Link } from 'react-router-dom';
+
+import PropTypes from 'prop-types';
+
 class MovieCard extends React.Component {
   render() {
+    const { movie } = this.props;
+    const { title, subtitle, storyline, id } = movie;
     return (
       <div data-testid="movie-card">
-        Movie Card
+        <span>
+          Título:
+          <span>
+            { title }
+          </span>
+        </span>
+        <span>
+          Subtítulo:
+          <span>
+            { subtitle }
+          </span>
+        </span>
+        <span>
+          Enredo:
+          <span>
+            { storyline }
+          </span>
+        </span>
+        <Link to={ `/movies/${id}` }>VER DETALHES</Link>
       </div>
     );
   }
 }
+
+MovieCard.propTypes = {
+  movie: PropTypes.shape({
+    title: PropTypes.string,
+    subtitle: PropTypes.string,
+    storyline: PropTypes.string,
+  }),
+}.isRequired;
 
 export default MovieCard;
