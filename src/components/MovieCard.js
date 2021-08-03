748d517a5403dff33c6c45ebe57be45e01eb1ca9
sonkai <mfilho86@gmail.com>
2021-08-03 18:01:16 -0300

    Req 1 corrigindo erros de lint que deixei passar

--- src/components/MovieCard.js
+++ src/components/MovieCard.js
@@ -1,10 +1,16 @@
 import React from 'react';
+// import PropTypes from 'prop-types';
 
 class MovieCard extends React.Component {
   render() {
+    // const { movie } = this.props;
+    // const { title, subtitle, storyline, imagePath } = movie;
     return (
       <div data-testid="movie-card">
-        Movie Card
+        {/* <h1 data-testid="movie-card-title">{ title }</h1>
+        <h2>{ subtitle }</h2>
+        <img src={ imagePath } alt="Capa do Filme" />
+        <p>{ storyline }</p> */}
       </div>
     );
   }
