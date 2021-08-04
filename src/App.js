9e6911273576b6cb0fe171d4846956d1f9c4bd51
Adriano Monteiro <adrianomonteiroweb@gmail.com>
2021-08-04 02:30:43 -0300

    requisito 1-2/5 e 3/5, rotas MovieDetails e NewMovie

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
 import React from 'react';
 import { BrowserRouter, Route } from 'react-router-dom';
 import MovieList from './pages/MovieList';
+import MovieDetails from './pages/MovieDetails';
+import NewMovie from './pages/NewMovie';
 
 function App() {
   return (
     <div>
       <BrowserRouter>
         <Route path="/" component={ MovieList } />
+        <Route path="/movies/:id" component={ MovieDetails } />
+        <Route path="/movies/new" component={ NewMovie } />
       </BrowserRouter>
     </div>
   );
