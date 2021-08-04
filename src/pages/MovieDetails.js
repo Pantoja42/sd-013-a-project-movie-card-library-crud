7db666ee8564717b0e84560c20ddde01d1ee9465
thomasravache <thomasravache31@gmail.com>
2021-08-04 12:01:06 -0300

    Ajustando lint do componente MovieDetails

--- src/pages/MovieDetails.js
+++ src/pages/MovieDetails.js
@@ -1,8 +1,8 @@
 import React, { Component } from 'react';
-
+import PropTypes from 'prop-types';
+import { Link } from 'react-router-dom';
 import * as movieAPI from '../services/movieAPI';
 import { Loading } from '../components';
-import { Link } from 'react-router-dom';
 
 class MovieDetails extends Component {
   constructor(props) {
@@ -10,39 +10,38 @@ class MovieDetails extends Component {
 
     this.state = {
       movie: false,
-    }
+    };
 
     this.fetchMovie = this.fetchMovie.bind(this);
     this.deleteMovie = this.deleteMovie.bind(this);
-  }
-
-  async fetchMovie() {
-    const { id } = this.props.match.params;
-
-    const movie = await movieAPI.getMovie(id);
-
-    this.setState({
-      movie,
-    })
-  }
-
-  async deleteMovie() {
-    const { id } = this.props.match.params;
-
-    const deletingMovie = await movieAPI.deleteMovie(id);
-
-    return deletingMovie;
   }
 
   componentDidMount() {
     this.fetchMovie();
   }
 
+  async fetchMovie() {
+    const { match: { params: { id } } } = this.props;
+
+    const movie = await movieAPI.getMovie(id);
+
+    this.setState({
+      movie,
+    });
+  }
+
+  async deleteMovie() {
+    const { match: { params: { id } } } = this.props;
+
+    const deletingMovie = await movieAPI.deleteMovie(id);
+
+    return deletingMovie;
+  }
+
   render() {
     // Change the condition to check the state
     // if (true) return <Loading />;
-
-    const { id } = this.props.match.params;
+    const { match: { params: { id } } } = this.props;
     const { movie } = this.state;
     const { title, storyline, imagePath, genre, rating, subtitle } = movie;
     const movieDetails = (
@@ -55,8 +54,8 @@ class MovieDetails extends Component {
         <p>{ `Rating: ${rating}` }</p>
         <hr />
         <div>
-          <Link to={ `/movies/${id}/edit` } >EDITAR</Link>
-          <Link to="/" onClick={ this.deleteMovie } >DELETAR</Link>
+          <Link to={ `/movies/${id}/edit` }>EDITAR</Link>
+          <Link to="/" onClick={ this.deleteMovie }>DELETAR</Link>
           <Link to="/">VOLTAR</Link>
         </div>
       </div>
@@ -67,5 +66,16 @@ class MovieDetails extends Component {
     );
   }
 }
+
+MovieDetails.propTypes = {
+  match: PropTypes.shape({
+    params: PropTypes.shape({
+      id: PropTypes.oneOfType([
+        PropTypes.string,
+        PropTypes.number,
+      ]).isRequired,
+    }),
+  }).isRequired,
+};
 
 export default MovieDetails;
