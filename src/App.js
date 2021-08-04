c58bec801b84bc3e594635f475d5f731229ae9d6
ivandosss <ivanndosssilva9389@gmail.com>
2021-08-04 15:09:14 -0300

    Requisito 1 - 5/5

--- src/App.js
+++ src/App.js
@@ -1,6 +1,10 @@
 import React from 'react';
 import { BrowserRouter, Route } from 'react-router-dom';
 import MovieList from './pages/MovieList';
+import MovieDetails from './pages/MovieDetails';
+import NewMovie from './pages/NewMovie';
+import EditMovie from './pages/EditMovie';
+import NotFound from './pages/NotFound';
 
 class App extends React.Component {
   render() {
@@ -8,6 +12,10 @@ class App extends React.Component {
       <div>
         <BrowserRouter>
           <Route path="/" component={ MovieList } />
+          <Route path="/movies/:id" component={ MovieDetails } />
+          <Route path="/movies/new" component={ NewMovie } />
+          <Route path="/movies/:id/edit" component={ EditMovie } />
+          <Route path="" component={ NotFound } />
         </BrowserRouter>
       </div>
     );
