f4c8e1d22deeced87ecae8bfaebfd00bd8bda5a5
Adriano Monteiro <adrianomonteiroweb@gmail.com>
2021-08-04 02:15:51 -0300

    Requisito 1-1/5 - rota raiz MovieList

--- src/App.js
+++ src/App.js
@@ -1,8 +1,14 @@
 import React from 'react';
+import { BrowserRouter, Route } from 'react-router-dom';
+import MovieList from './pages/MovieList';
 
 function App() {
   return (
-    <div>Movie Card Library CRUD</div>
+    <div>
+      <BrowserRouter>
+        <Route path="/" component={ MovieList } />
+      </BrowserRouter>
+    </div>
   );
 }
 
