9c93e6099a7b5074a94ef194fb2554315885108a
Adriano Monteiro <adrianomonteiroweb@gmail.com>
2021-08-04 02:35:42 -0300

    requisito 1-4/5 - rota EditMovie

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { BrowserRouter, Route } from 'react-router-dom';
 import MovieList from './pages/MovieList';
 import MovieDetails from './pages/MovieDetails';
 import NewMovie from './pages/NewMovie';
+import EditMovie from './pages/EditMovie';
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
         <Route path="/" component={ MovieList } />
         <Route path="/movies/:id" component={ MovieDetails } />
         <Route path="/movies/new" component={ NewMovie } />
+        <Route path="/movies/:id/edit" component={ EditMovie } />
       </BrowserRouter>
     </div>
   );
