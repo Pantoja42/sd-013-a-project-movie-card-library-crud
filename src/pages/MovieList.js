47479ca6d291fbc5ae8e28c4da05ab9d1a2690f5
Bruno <brunoalencarlima@outlook.com.br>
2021-08-05 17:22:47 -0300

    movielist

--- src/pages/MovieList.js
+++ src/pages/MovieList.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import MovieCard from '../components/MovieCard';
-
-import * as movieAPI from '../services/movieAPI';
+import { getMovies } from '../services/movieAPI';
+import Loading from '../components/Loading';
 
 class MovieList extends Component {
   constructor() {
@@ -9,17 +9,32 @@ class MovieList extends Component {
 
     this.state = {
       movies: [],
+      loading: true,
     };
   }
 
+  componentDidMount() {
+    this.fetch(getMovies);
+  }
+
+  async fetch(newGetMovies) {
+    const receivePromisse = await newGetMovies();
+    this.setState({
+      movies: Object.values(receivePromisse),
+      loading: false,
+    });
+  }
+
   render() {
-    const { movies } = this.state;
-
-    // Render Loading here if the request is still happening
-
+    const { movies, loading } = this.state;
+    const show = () => {
+      const verifyState = (loading) ? <Loading /> : undefined;
+      return verifyState;
+    };
     return (
       <div data-testid="movie-list">
         {movies.map((movie) => <MovieCard key={ movie.title } movie={ movie } />)}
+        { show() }
       </div>
     );
   }
