dec3cea65f6d6969ec0783dc3b992f518903de0e
Japa <wesleyy_@live.com>
2021-08-05 19:26:39 -0300

    Adicionado requisicao ao MovieDetails, loading e renderizaçao apos requisicao

--- src/pages/MovieDetails.js
+++ src/pages/MovieDetails.js
@@ -1,25 +1,74 @@
 import React, { Component } from 'react';
-
-import * as movieAPI from '../services/movieAPI';
+import PropTypes from 'prop-types';
+import { Link } from 'react-router-dom';
 import { Loading } from '../components';
 
+import * as movieAPI from '../services/movieAPI';
+
 class MovieDetails extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      movie: [],
+      loading: true,
+    };
+  }
+
+  componentDidMount() {
+    const { match: { params: { id } } } = this.props;
+    movieAPI.getMovie(id).then((data) => {
+      this.setState({
+        movie: data,
+        loading: false,
+      });
+    });
+  }
+
   render() {
-    // Change the condition to check the state
-    // if (true) return <Loading />;
+    const { loading } = this.state;
+    const { movie: {
+      title,
+      storyline,
+      imagePath,
+      genre,
+      rating,
+      subtitle,
+      id } } = this.state;
 
-    const { title, storyline, imagePath, genre, rating, subtitle } = {};
-
-    return (
+    const movieDetails = (
       <div data-testid="movie-details">
         <img alt="Movie Cover" src={ `../${imagePath}` } />
-        <p>{ `Subtitle: ${subtitle}` }</p>
-        <p>{ `Storyline: ${storyline}` }</p>
-        <p>{ `Genre: ${genre}` }</p>
-        <p>{ `Rating: ${rating}` }</p>
+        <div>
+          <h2>{ `Title: ${title}` }</h2>
+          <h3>{ `Subtitle: ${subtitle}` }</h3>
+          <p>{ `Storyline: ${storyline}` }</p>
+          <p>{ `Genre: ${genre}` }</p>
+          <p>{ `Rating: ${rating}` }</p>
+          <div>
+            <Link to={ `/movies/${id}/edit` }>EDITAR</Link>
+          </div>
+          <div>
+            <Link to="/">VOLTAR</Link>
+          </div>
+        </div>
+      </div>
+    );
+
+    return (
+      <div>
+        {loading ? <Loading /> : movieDetails}
       </div>
     );
   }
 }
+
+MovieDetails.propTypes = {
+  match: PropTypes.shape({
+    params: PropTypes.shape({
+      id: PropTypes.string,
+    }),
+  }).isRequired,
+};
 
 export default MovieDetails;
