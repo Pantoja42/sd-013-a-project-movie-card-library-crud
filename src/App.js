432d9f78b6ab4c84109b56f14dfcd2a64b15823a
Breno Lopes <brenokos4@gmail.com>
2021-08-05 17:44:16 -0300

    add requsito 6 uhuuuuuuuuuul

--- src/App.js
+++ src/App.js
@@ -4,23 +4,25 @@ import { MovieList, MovieDetails, NewMovie, EditMovie, NotFound } from './pages'
 
 function App() {
   return (
-    <BrowserRouter>
-      <Switch>
-        <Route exact path="/" component={ MovieList } />
-        <Route exact path="/movies/new" component={ NewMovie } />
-        <Route
-          exact
-          path="/movies/:id"
-          render={ (props) => <MovieDetails { ...props } /> }
-        />
-        <Route
-          exact
-          path="/movies/:id/edit"
-          render={ (props) => <EditMovie { ...props } /> }
-        />
-        <Route exact component={ NotFound } />
-      </Switch>
-    </BrowserRouter>
+    <div>
+      <BrowserRouter>
+        <Switch>
+          <Route exact path="/" component={ MovieList } />
+          <Route exact path="/movies/new" component={ NewMovie } />
+          <Route
+            exact
+            path="/movies/:id"
+            render={ (props) => <MovieDetails { ...props } /> }
+          />
+          <Route
+            exact
+            path="/movies/:id/edit"
+            render={ (props) => <EditMovie { ...props } /> }
+          />
+          <Route exact component={ NotFound } />
+        </Switch>
+      </BrowserRouter>
+    </div>
   );
 }
 
