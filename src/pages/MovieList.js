4ac5a6dd293d68acb68b96051ffcd5d06b45468e
Jean V  Pacheco <jeanvitorp@gmail.com>
2021-08-04 22:39:13 -0300

    Adiciona requisito 6

--- src/pages/MovieList.js
+++ src/pages/MovieList.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
 import { Loading } from '../components';
 import MovieCard from '../components/MovieCard';
 import * as movieAPI from '../services/movieAPI';
@@ -26,7 +27,12 @@ class MovieList extends Component {
     return (
       <div data-testid="movie-list">
         { loading ? <Loading />
-          : movies.map((movie) => <MovieCard key={ movie.title } movie={ movie } />)}
+          : (
+            <div>
+              {movies.map((movie) => <MovieCard key={ movie.title } movie={ movie } />)}
+              <Link to="/movies/new">ADICIONAR CARTÃO</Link>
+            </div>
+          )}
       </div>
     );
   }
