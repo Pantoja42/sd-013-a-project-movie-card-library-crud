689e520b3a1a494ab0a5792f4fc22af8a0d330f6
Diego Vilela <vilelaphp@gmail.com>
2021-08-05 17:21:38 -0300

    Implement requirement 1

--- src/App.js
+++ src/App.js
@@ -1,9 +1,31 @@
-import React from 'react';
+import React, { Component } from 'react';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import EditMovie from './pages/EditMovie';
+import MovieDetails from './pages/MovieDetails';
+import MovieList from './pages/MovieList';
+import NewMovie from './pages/NewMovie';
+import NotFound from './pages/NotFound';
 
-function App() {
-  return (
-    <div>Movie Card Library CRUD</div>
-  );
+class App extends Component {
+  render() {
+    return (
+      <BrowserRouter>
+        <Switch>
+          <Route exact path="/" component={ MovieList } />
+          <Route path="/movies/new" component={ NewMovie } />
+          <Route
+            path="/movies/:id/edit"
+            render={ ({ match }) => <EditMovie match={ match } /> }
+          />
+          <Route
+            path="/movies/:id"
+            render={ ({ match }) => <MovieDetails match={ match } /> }
+          />
+          <Route component={ NotFound } />
+        </Switch>
+      </BrowserRouter>
+    );
+  }
 }
 
 export default App;
