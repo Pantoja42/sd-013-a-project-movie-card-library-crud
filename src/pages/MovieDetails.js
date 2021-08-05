fefc06f1710d5e4fec550cf79a36fcb8ede4a445
Breno Lopes <brenokos4@gmail.com>
2021-08-05 16:50:51 -0300

    requisito 4! (nao passa no troço de :id)

--- src/pages/MovieDetails.js
+++ src/pages/MovieDetails.js
@@ -1,22 +1,57 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
 
 import * as movieAPI from '../services/movieAPI';
 import { Loading } from '../components';
 
 class MovieDetails extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      movie: {},
+      loading: true,
+    };
+
+    this.fetchMovieById = this.fetchMovieById.bind(this);
+  }
+
+  componentDidMount() {
+    this.fetchMovieById();
+  }
+
+  async fetchMovieById() {
+    const { match } = this.props;
+    const { id } = match.params;
+
+    const movie = await movieAPI.getMovie(id);
+
+    await this.setState(() => ({
+      loading: false,
+      movie,
+    }));
+  }
+
   render() {
-    // Change the condition to check the state
-    // if (true) return <Loading />;
+    const { movie, loading } = this.state;
+    const { storyline, imagePath, genre, rating, subtitle } = movie;
 
-    const { title, storyline, imagePath, genre, rating, subtitle } = {};
+    console.log(movie);
 
+    if (loading) {
+      return <Loading />;
+    }
     return (
-      <div data-testid="movie-details">
-        <img alt="Movie Cover" src={ `../${imagePath}` } />
-        <p>{ `Subtitle: ${subtitle}` }</p>
-        <p>{ `Storyline: ${storyline}` }</p>
-        <p>{ `Genre: ${genre}` }</p>
-        <p>{ `Rating: ${rating}` }</p>
+      <div>
+        <div data-testid="movie-details">
+          <img alt="Movie Cover" src={ `../${imagePath}` } />
+          <p>{ `Subtitle: ${subtitle}` }</p>
+          <p>{ `Storyline: ${storyline}` }</p>
+          <p>{ `Genre: ${genre}` }</p>
+          <p>{ `Rating: ${rating}` }</p>
+          <Link to="/">VOLTAR</Link>
+        </div>
+        <Link to="/movies/:id/edit">EDITAR</Link>
       </div>
     );
   }
