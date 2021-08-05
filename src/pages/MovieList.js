380b4f1f469211bf0e01ee9530fb317833c7345d
Bruno Pinho <brunopinho@outlook.com>
2021-08-05 20:53:50 -0300

    feito requisito 6

--- src/pages/MovieList.js
+++ src/pages/MovieList.js
@@ -1,37 +1,37 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
 import MovieCard from '../components/MovieCard';
-import Loading from '../components/Loading'
+import Loading from '../components/Loading';
 import * as movieAPI from '../services/movieAPI';
 
 class MovieList extends Component {
   constructor() {
     super();
-    
+
     this.state = {
       movies: [],
     };
   }
-  
+
   componentDidMount() {
     // com a ajuda do Vinicius Dionysio e Sugano na monitoria
     const { getMovies } = movieAPI;
     getMovies().then((list) => {
       this.setState({ movies: list });
-    })
+    });
   }
 
   render() {
     const { movies } = this.state;
 
-    if (this.state.movies.length === 0) {
-      return (
-        <Loading />
-      );
-    }
-    
+    if (movies.length === 0) return (<Loading />);
+
     return (
       <div data-testid="movie-list">
         {movies.map((movie) => <MovieCard key={ movie.title } movie={ movie } />)}
+        <p>
+          <Link to="/movies/new">ADICIONAR CARTÃO</Link>
+        </p>
       </div>
     );
   }
