6052d61a0510cd0dc77fb3c630bef1a17f110593
Diego Vilela <vilelaphp@gmail.com>
2021-08-05 19:10:30 -0300

    Implement requirement 5

--- src/pages/EditMovie.js
+++ src/pages/EditMovie.js
@@ -1,27 +1,44 @@
 import React, { Component } from 'react';
-
-import { MovieForm } from '../components';
+import { Redirect } from 'react-router-dom';
+import { Loading, MovieForm } from '../components';
 import * as movieAPI from '../services/movieAPI';
 
 class EditMovie extends Component {
   constructor(props) {
     super(props);
-    this.state = {};
+    this.state = {
+      shouldRedirect: false,
+      loading: true,
+      movie: [],
+    };
     this.handleSubmit = this.handleSubmit.bind(this);
   }
 
+  async componentDidMount() {
+    const { match: { params: { id } } } = this.props;
+
+    const movie = await movieAPI.getMovie(id);
+    this.keepMovie(movie);
+  }
+
   handleSubmit(updatedMovie) {
+    movieAPI.updateMovie(updatedMovie);
+    this.setState({ shouldRedirect: true });
+  }
+
+  keepMovie(movie) {
+    this.setState({
+      movie,
+      loading: false,
+    });
   }
 
   render() {
-    const { status, shouldRedirect, movie } = this.state;
-    if (shouldRedirect) {
-      // Redirect
-    }
+    const { loading, shouldRedirect, movie } = this.state;
 
-    if (status === 'loading') {
-      // render Loading
-    }
+    if (shouldRedirect) return <Redirect to="/" />;
+
+    if (loading) return <Loading />;
 
     return (
       <div data-testid="edit-movie">
