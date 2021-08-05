09f6f33de8968ddc734c73fca5ba33029115ad18
LuanRodriguesep <luanrodriguesep@yahoo.com.br>
2021-08-05 19:13:01 -0300

    requisito 03

--- src/components/MovieCard.js
+++ src/components/MovieCard.js
@@ -1,13 +1,29 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import PropTypes from 'prop-types';
 
 class MovieCard extends React.Component {
   render() {
+    const { movie } = this.props;
     return (
       <div data-testid="movie-card">
-        Movie Card
+        <p>{ movie.title }</p>
+        <img src={ movie.imagePath } alt="Capa do Filme" />
+        <p>{ movie.storyline }</p>
+        <Link to={ `movies/${movie.id}` }>VER DETALHES</Link>
       </div>
     );
   }
 }
+
+// Apos rever a aula aoVivo 13.02 no minuto 38.25 por algum tempo, consegui resolver esse requisito
+
+MovieCard.propTypes = {
+  movie: PropTypes.objectOf(
+    PropTypes.string,
+    PropTypes.bool,
+    PropTypes.number,
+  ).isRequired,
+};
 
 export default MovieCard;
