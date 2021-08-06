bb44a415ac595fe1a192287a6d266ab9be7673b5
junglejf <jfcchagas@id.uff.br>
2021-08-06 01:51:35 -0300

    Requisito 4- Implementado Requisição de Movie e os Botões de Editar e Voltar

--- src/pages/MovieDetails.js
+++ src/pages/MovieDetails.js
@@ -1,22 +1,54 @@
 import React, { Component } from 'react';
 
+import { NavLink, Link } from 'react-router-dom';
 import * as movieAPI from '../services/movieAPI';
 import { Loading } from '../components';
 
 class MovieDetails extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      movie: {},
+      loading: false,
+    };
+  }
+
+  componentDidMount() {
+    this.fetchMovie();
+  }
+
+  async fetchMovie() {
+    this.setState(
+      { loading: true },
+      async () => {
+        const { match: { params: { id } } } = this.props;
+        const requestMovie = await movieAPI.getMovie(id);
+        this.setState({ movie: requestMovie, loading: false });
+      },
+    );
+  }
+
+  // preventDefault = (e) => e.preventDefault
   render() {
-    // Change the condition to check the state
-    // if (true) return <Loading />;
-
-    const { title, storyline, imagePath, genre, rating, subtitle } = {};
-
+    const { loading, movie } = this.state;
+    const { id, title, storyline, imagePath, genre, rating, subtitle } = movie;
+    // console.log(movie.id);
+    if (loading) { return <Loading />; }
     return (
       <div data-testid="movie-details">
         <img alt="Movie Cover" src={ `../${imagePath}` } />
+        <p>{ `title: ${title}` }</p>
         <p>{ `Subtitle: ${subtitle}` }</p>
         <p>{ `Storyline: ${storyline}` }</p>
         <p>{ `Genre: ${genre}` }</p>
         <p>{ `Rating: ${rating}` }</p>
+        <div className="buttonContainer">
+          <NavLink to="/" className="buttonLink"> VOLTAR </NavLink>
+          <NavLink to={ `/movies/${id}/edit` } className="buttonLink">
+            EDITAR
+          </NavLink>
+        </div>
       </div>
     );
   }
