6e7687cadc24a473dc3189dcfaa5ef0aab448341
Andrey Gois <gois.andrey@hotmail.com>
2021-08-06 16:40:47 -0300

    Adiciona questao 01 e 02, sem contagem, pois o lint não ira permitir

--- src/App.js
+++ src/App.js
@@ -1,8 +1,29 @@
 import React from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import './App.css';
+import { EditMovie, MovieDetails, MovieList, NewMovie, NotFound } from './pages';
 
 function App() {
   return (
-    <div>Movie Card Library CRUD</div>
+    <Router>
+      <Switch>
+        <Route exact path="/">
+          <MovieList />
+        </Route>
+        <Route exact path="/movies/new">
+          <NewMovie />
+        </Route>
+        <Route exact path="/movies/:id">
+          <MovieDetails />
+        </Route>
+        <Route exact path="/movies/:id/edit">
+          <EditMovie />
+        </Route>
+        <Route path="*">
+          <NotFound />
+        </Route>
+      </Switch>
+    </Router>
   );
 }
 
