b5929a7c700f7571d19b8f3c2b4841b2fc4150fd
J Victor Bap <dixxbaptista@gmail.com>
2021-08-05 22:25:18 -0300

    ate ex 3 sem erros lint

--- src/pages/MovieDetails.js
+++ src/pages/MovieDetails.js
@@ -1,23 +1,32 @@
 import React, { Component } from 'react';
+// import { Link } from 'react-router-dom';
 
-import * as movieAPI from '../services/movieAPI';
+// import * as movieAPI from '../services/movieAPI';
 import { Loading } from '../components';
 
 class MovieDetails extends Component {
-  render() {
-    // Change the condition to check the state
-    // if (true) return <Loading />;
+  constructor(props) {
+    super(props);
 
-    const { title, storyline, imagePath, genre, rating, subtitle } = {};
+    this.state = {
+      movie: {},
+      loading: true,
+    };
+  }
+
+  componentDidMount() {
+    this.LoadMovie();
+  }
+
+  async LoadMovie() {
+    // const info = movieAPI.getMovie(id);
+  }
+
+  render() {
+    const { loading, movie } = this.state;
 
     return (
-      <div data-testid="movie-details">
-        <img alt="Movie Cover" src={ `../${imagePath}` } />
-        <p>{ `Subtitle: ${subtitle}` }</p>
-        <p>{ `Storyline: ${storyline}` }</p>
-        <p>{ `Genre: ${genre}` }</p>
-        <p>{ `Rating: ${rating}` }</p>
-      </div>
+      loading ? <Loading /> : this.LoadMovie(movie)
     );
   }
 }
