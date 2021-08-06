fd524e44e728493db65d7fdf462f744b47d0b411
PC <sthecso>
2021-08-06 20:04:23 -0300

    Requisito 3 corrigido

--- src/components/MovieCard.js
+++ src/components/MovieCard.js
@@ -20,7 +20,7 @@ MovieCard.propTypes = {
   movie: PropTypes.shape({
     title: PropTypes.string,
     storyline: PropTypes.string,
-    id: PropTypes.string,
+    id: PropTypes.number,
   }).isRequired,
 };
 
