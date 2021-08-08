84be975950e2ced675fa42644d8dc30f67e0781f
Luiz Casimiro <luizcasimi@gmail.com>
2021-08-07 22:09:07 -0300

    solve 2

--- src/App.js
+++ src/App.js
@@ -1,13 +1,22 @@
-import React from 'react';
-import { BrowserRouter as Router } from 'react-router-dom';
+import React, { Component } from 'react';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import { MovieList, MovieDetails, NewMovie, NotFound, EditMovie } from './pages';
 import './App.css';
 
-function App() {
-  return (
-    <Router>
-      <div>Movie Card Library CRUD</div>
-    </Router>
-  );
+class App extends Component {
+  render() {
+    return (
+      <BrowserRouter>
+        <Switch>
+          <Route exact path="/movies/new" component={ NewMovie } />
+          <Route exact path="/movies/:id/edit" component={ EditMovie } />
+          <Route exact path="/movies/:id" component={ MovieDetails } />
+          <Route exact path="/" component={ MovieList } />
+          <Route component={ NotFound } />
+        </Switch>
+      </BrowserRouter>
+    );
+  }
 }
 
 export default App;
--- src/pages/MovieList.js
+++ src/pages/MovieList.js
@@ -1,28 +1,47 @@
-// import React, { Component } from 'react';
-// import MovieCard from '../components/MovieCard';
+import React, { Component } from 'react';
+import MovieCard from '../components/MovieCard';
+import { Loading } from '../components';
 
-// import * as movieAPI from '../services/movieAPI';
+import * as movieAPI from '../services/movieAPI';
 
-// class MovieList extends Component {
-//   constructor() {
-//     super();
+class MovieList extends Component {
+  constructor() {
+    super();
 
-//     this.state = {
-//       movies: [],
-//     };
-//   }
+    this.state = {
+      movies: [],
+      loading: true,
+    };
+  }
 
-//   render() {
-//     const { movies } = this.state;
+  // requisito 2 ================
+  componentDidMount() {
+    this.requestAPI();
+  }
 
-//     // Render Loading here if the request is still happening
+  requestAPI = async () => {
+    const getFakeAPI = await movieAPI.getMovies();
+    this.setState({ movies: getFakeAPI, loading: false });
+  }
 
-//     return (
-//       <div data-testid="movie-list">
-//         {movies.map((movie) => <MovieCard key={ movie.title } movie={ movie } />)}
-//       </div>
-//     );
-//   }
-// }
+  // fim requisito 2 ================
 
-// export default MovieList;
+  render() {
+    const { movies, loading } = this.state;
+
+    // requisito 2 ================
+    // Render Loading here if the request is still happening
+    if (loading) {
+      return <Loading />;
+    }
+    // fim requisito 2 ================
+
+    return (
+      <div data-testid="movie-list">
+        {movies.map((movie) => <MovieCard key={ movie.title } movie={ movie } />)}
+      </div>
+    );
+  }
+}
+
+export default MovieList;
