d81c889e2037a7a3fbbcf208cce43d0aedc99c70
Caê <eduardokae01@hotmail.com>
2021-08-08 20:43:40 -0300

    requisito 2

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter, Route } from 'react-router-dom';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import EditMovie from './pages/EditMovie';
 import MovieDetails from './pages/MovieDetails';
 import MovieList from './pages/MovieList';
@@ -9,11 +9,13 @@ import NotFound from './pages/NotFound';
 function App() {
   return (
     <BrowserRouter>
-      <Route path="/" component={ MovieList } />
-      <Route path="/movies/:id" component={ MovieDetails } />
-      <Route path="/movies/new" component={ NewMovie } />
-      <Route path="/movies/:id/edit" component={ EditMovie } />
-      <Route path="" component={ NotFound } />
+      <Switch>
+        <Route exact path="/" component={ MovieList } />
+        <Route path="/movies/new" component={ NewMovie } />
+        <Route path="/movies/:id" component={ MovieDetails } />
+        <Route path="/movies/:id/edit" component={ EditMovie } />
+        <Route path="" component={ NotFound } />
+      </Switch>
     </BrowserRouter>
   );
 }
--- src/components/MovieCard.js
+++ src/components/MovieCard.js
@@ -2,6 +2,8 @@ import React from 'react';
 
 class MovieCard extends React.Component {
   render() {
+    // const { movie } = this.props;
+
     return (
       <div data-testid="movie-card">
         Movie Card
