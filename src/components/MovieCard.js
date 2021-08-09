5b6856267e20f627d3457e4c434b87049696629d
Caê <eduardokae01@hotmail.com>
2021-08-08 21:08:24 -0300

    add prop types em movieCard

--- src/components/MovieCard.js
+++ src/components/MovieCard.js
@@ -1,19 +1,28 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import PropTypes from 'prop-types';
 
 class MovieCard extends React.Component {
   render() {
-    const { movie: { title, storyline, id } } = this.props;
-    // const { title, storyline, id } = movie;
+    const { movie: { title, storyline, id, imagePath } } = this.props;
 
     return (
       <div data-testid="movie-card">
         <h1>{ title }</h1>
         <p>{ storyline }</p>
+        <img src={ imagePath } alt={ title } />
         <Link to={ `movies/${id}` }>VER DETALHES</Link>
       </div>
     );
   }
 }
+
+MovieCard.propTypes = {
+  movie: PropTypes.shape,
+  title: PropTypes.string,
+  storyline: PropTypes.string,
+  id: PropTypes.number,
+  imagePath: PropTypes.string,
+}.isRequired;
 
 export default MovieCard;
