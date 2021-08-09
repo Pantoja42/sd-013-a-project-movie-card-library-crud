edd17f658146251b9fe350026feee477741dc600
Matheus Camillo <matheuscamillo@hotmail.com.br>
2021-08-09 05:44:30 -0300

    resolvido movieCard

--- src/components/MovieCard.js
+++ src/components/MovieCard.js
@@ -1,10 +1,23 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 class MovieCard extends React.Component {
   render() {
+
+    const { movie: {
+      id,
+      title,
+      storyline,
+      imagePath,
+    }
+  } = this.props;
+
     return (
       <div data-testid="movie-card">
-        Movie Card
+        <img src={ imagePath } alt={ title } />
+        <h1>{ title }</h1>
+        <h3>{ storyline }</h3>
+        <Link to={ `/movies/${id}` }>VER DETALHES</Link>
       </div>
     );
   }
