7377da7b7d26463d7d37822396723f1cc20d5d4d
Jaqueline Silva <jaqueline.natario@gmail.com>
2021-08-09 11:45:06 -0300

    Resolve o requisito 1

--- src/App.js
+++ src/App.js
@@ -1,13 +1,40 @@
-import React from 'react';
-import { BrowserRouter as Router } from 'react-router-dom';
-import './App.css';
+// Requisito 1 - Renderize BrowserRouter no componente App usando rotas.
+// Você deve utilizar um BrowserRouter pra criar as rotas da sua aplicação.
+// As urls de cada página devem ser desenvolvidas conforme especificado na seção O que será desenvolvido.
 
-function App() {
-  return (
-    <Router>
-      <div>Movie Card Library CRUD</div>
-    </Router>
-  );
+// O que será verificado:
+// Será validado se a rota / renderiza a página MovieList
+// Será validado se a rota /movies/:id renderiza a página MovieDetails
+// Será validado se a rota /movies/new renderiza a página NewMovie
+// Será validado se a rota /movies/:id/edit renderiza a página EditMovie
+// Será validado se qualquer rota não declarada renderiza a página NotFound
+
+// ============================
+
+import React from 'react';
+import { BrowserRouter, Route } from 'react-router-dom';
+import MovieList from './pages/MovieList';
+import MovieDetails from './pages/MovieDetails';
+import NewMovie from './pages/NewMovie';
+import EditMovie from './pages/EditMovie';
+import NotFound from './pages/NotFound';
+// import './App.css';
+
+class App extends React.Component {
+  render() {
+    return (
+      <div>
+        <div>Movie Card Library CRUD</div>
+        <BrowserRouter>
+          <Route exact path="/" component={ MovieList } />
+          <Route exact path="/movies/:id" component={ MovieDetails } />
+          <Route exact path="/movies/new" component={ NewMovie } />
+          <Route exact path="/movies/:id/edit" component={ EditMovie } />
+          <Route component={ NotFound } />
+        </BrowserRouter>
+      </div>
+    );
+  }
 }
 
 export default App;
