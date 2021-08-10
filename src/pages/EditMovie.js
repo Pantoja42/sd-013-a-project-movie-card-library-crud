b53e0c4e4c138bd929348dd9ac4e74347bfebde0
deyvid-dev <deyvidscm@hotmail.com>
2021-08-10 11:50:21 -0300

    requisito 5

--- src/pages/EditMovie.js
+++ src/pages/EditMovie.js
@@ -1,26 +1,57 @@
 import React, { Component } from 'react';
+import { Redirect } from 'react-router';
+import PropTypes from 'prop-types';
+import { Loading, MovieForm } from '../components';
+import * as movieAPI from '../services/movieAPI';
 
-import { MovieForm } from '../components';
-// import * as movieAPI from '../services/movieAPI';
+// Criado com a ajuda do aluno Murilo Rainho
 
 class EditMovie extends Component {
   constructor(props) {
     super(props);
-    this.state = {};
+    this.state = {
+      movie: {},
+      shouldRedirect: false,
+      status: 'loading',
+    };
     this.handleSubmit = this.handleSubmit.bind(this);
   }
 
-  //  handleSubmit(updatedMovie) {
-  //  }
+  componentDidMount() {
+    this.catchMovie();
+  }
+
+  handleSubmit(updatedMovie) {
+    movieAPI.updateMovie(updatedMovie);
+    this.setState({ shouldRedirect: true });
+  }
+
+  catchMovie = async () => {
+    const { match: { params: { id } } } = this.props;
+    const findMovie = await movieAPI.getMovie(id);
+
+    this.setState({ status: 'loading' }, () => {
+      this.setState({
+        movie: findMovie,
+        status: '',
+      });
+    });
+  }
 
   render() {
     const { status, shouldRedirect, movie } = this.state;
     if (shouldRedirect) {
       // Redirect
+      return (
+        <Redirect exact path="/" />
+      );
     }
 
     if (status === 'loading') {
       // render Loading
+      return (
+        <Loading />
+      );
     }
 
     return (
@@ -30,5 +61,13 @@ class EditMovie extends Component {
     );
   }
 }
+
+EditMovie.propTypes = {
+  match: PropTypes.shape({
+    params: PropTypes.shape({
+      id: PropTypes.string,
+    }),
+  }).isRequired,
+};
 
 export default EditMovie;
