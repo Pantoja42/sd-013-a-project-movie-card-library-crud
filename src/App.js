ac30c014a05991eef5fd43fa12e6d7d1be0deb7f
gbezzuoli <guilhermebezzuoli@outlook.com>
2021-08-13 14:10:08 -0300

    commitão

--- src/App.js
+++ src/App.js
@@ -1,11 +1,42 @@
 import React from 'react';
-import { BrowserRouter as Router} from 'react-router-dom'
-import './App.cs'
-
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import MovieList from './pages/MovieList';
+import MovieDetails from './pages/MovieDetails';
+import NewMovie from './pages/NewMovie';
+import EditMovie from './pages/EditMovie';
+import NotFoud from './pages/NotFound';
+import './App.css';
 
 function App() {
   return (
-    <div>Movie Card Library CRUD</div>
+    <div>
+      Movie Card Library CRUD
+      <Router>
+        <Switch>
+          <Route
+            exact
+            path="/"
+            component={ MovieList }
+          />
+          <Route
+            path="/movies/:id/edit"
+            component={ EditMovie }
+          />
+          <Route
+            path="/movies/new"
+            component={ NewMovie }
+          />
+          <Route
+            path="/movies/:id"
+            component={ MovieDetails }
+          />
+          <Route
+            path="*"
+            component={ NotFoud }
+          />
+        </Switch>
+      </Router>
+    </div>
   );
 }
 
