c9e0e4be020f06ee5ac29221f5a52d0ca4df8ebb
Rafael Araujo <rafacarlosaraujo@gmail.com>
2021-08-27 01:49:00 -0300

    Requisito 1

--- src/App.js
+++ src/App.js
@@ -1,11 +1,35 @@
+/*
+1 - Renderize BrowserRouter no componente App usando rotas
+Você deve utilizar um BrowserRouter pra criar as rotas da sua aplicação. As urls de cada página devem ser desenvolvidas conforme especificado na seção O que será desenvolvido.
+
+O que será verificado:
+Será validado se a rota / renderiza a página MovieList
+
+Será validado se a rota /movies/:id renderiza a página MovieDetails
+
+Será validado se a rota /movies/new renderiza a página NewMovie
+
+Será validado se a rota /movies/:id/edit renderiza a página EditMovie
+
+Será validado se qualquer rota não declarada renderiza a página NotFound
+*/
+
 import React from 'react';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import { EditMovie, MovieDetails, MovieList, NewMovie, NotFound } from './pages';
 
 function App() {
   return (
-    <div>
-      <p>Movie Card Library CRUD</p>
-      <p>Teste</p>
-    </div>
+    <BrowserRouter>
+      <div>Movie Card Library CRUD</div>
+      <Switch>
+        <Route exact path="/" component={ MovieList } />
+        <Route path="/movies/:id" component={ MovieDetails } />
+        <Route path="/movies/new" component={ NewMovie } />
+        <Route path="/movies/:id/edit" component={ EditMovie } />
+        <Route component={ NotFound } />
+      </Switch>
+    </BrowserRouter>
   );
 }
 
