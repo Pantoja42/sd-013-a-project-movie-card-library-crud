602bb699e842d4e687d2e2e66593d2aa69c4de78
Matheus Macedo <matheus.castro.macedo.xi@gmail.com>
2021-08-26 22:00:39 -0300

    Requisito 6

--- src/pages/MovieList.js
+++ src/pages/MovieList.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
 import { Loading } from '../components';
 import MovieCard from '../components/MovieCard';
 import * as movieAPI from '../services/movieAPI';
@@ -26,12 +27,15 @@ class MovieList extends Component {
     const { movies, loading } = this.state;
 
     // Render Loading here if the request is still happening
+    if (loading) return <Loading />;
 
     return (
       <div className="movie-list" data-testid="movie-list">
+        <Link to="/movies/new">
+          ADICIONAR CARTÃO
+        </Link>
         {
-          loading ? <Loading />
-            : movies.map((movie) => <MovieCard key={ movie.title } movie={ movie } />)
+          movies.map((movie) => <MovieCard key={ movie.title } movie={ movie } />)
         }
       </div>);
   }
