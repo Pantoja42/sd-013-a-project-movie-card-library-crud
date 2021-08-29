2a81bd2e5d20662f805154fc78cd009dc63eb727
Gabriel Priss <gabrielpriss@hotmail.com>
2021-08-29 14:54:40 -0300

    requisitos 1 e 2

--- src/App.js
+++ src/App.js
@@ -1,11 +1,22 @@
 import React from 'react';
-import { BrowserRouter as Router } from 'react-router-dom';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import './App.css';
+import { EditMovie, MovieList, MovieDetails, NewMovie, NotFound } from './pages';
+
 
 function App() {
   return (
     <Router>
-      <div>Movie Card Library CRUD</div>
+      <div>
+        Movie Card Library CRUD
+        <Switch>
+          <Route exact path="/" component={ MovieList } />
+          <Route exact path="/movies/:id" component={ MovieDetails } />
+          <Route exact path="/movies/new" component={ NewMovie } />
+          <Route exact path="/movies/:id/edit" component={ EditMovie } />
+          <Route path={ undefined } component={ NotFound } />
+        </Switch>
+      </div>
     </Router>
   );
 }
