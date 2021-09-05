e12330022915dac6542b890edbf3d282f0e12f8c
Walace Nascimento <wlacens@hotmail.com>
2021-09-05 15:32:47 -0300

    Resolve o requisito 1

--- src/App.js
+++ src/App.js
@@ -1,9 +1,35 @@
-import React from 'react';
+import React, { Component } from 'react';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import MovieList from './pages/MovieList';
+import MovieDetails from './pages/MovieDetails';
+import NewMovie from './pages/NewMovie';
+import EditMovie from './pages/EditMovie';
+import NotFound from './pages/NotFound';
 
-function App() {
-  return (
-    <div>Movie Card Library CRUD</div>
-  );
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <div> Movie Card Library CRUD </div>
+        <BrowserRouter>
+          <Switch>
+            <Route path="/movies/new" component={ NewMovie } />
+            <Route path="/movies/:id/edit" component={ EditMovie } />
+            <Route path="/movies/:id" component={ MovieDetails } />
+            <Route exact path="/" component={ MovieList } />
+            <Route path="*" component={ NotFound } />
+
+            {/* <Route path="/movies/new" component={ NewMovie } />
+            <Route path="/movies/:id/edit" component={ EditMovie } />
+            <Route exact path="/" component={ MovieList } />
+            <Route path="/movies/:id" component={ MovieDetails } />
+            <Route path="*" component={ NotFound } /> */}
+
+          </Switch>
+        </BrowserRouter>
+      </div>
+    );
+  }
 }
 
 export default App;
