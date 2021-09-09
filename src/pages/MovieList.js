1908220937faa5944091faef3f8c3f0dbffe98ab
Gean634N <gean.a.l.c@gmail.com>
2021-09-09 07:34:54 -0400

    :up: requisito 02

--- src/pages/MovieList.js
+++ src/pages/MovieList.js
@@ -22,7 +22,7 @@ class MovieList extends Component {
     if (loading) return <Loading />; // Render Loading here if the request is still happening
 
     return (
-      <div data-testid="movie-list">
+      <div data-testid="movie-list" className="movie-list">
         {movies.map((movie) => <MovieCard key={ movie.id } movie={ movie } />)}
       </div>
     );
