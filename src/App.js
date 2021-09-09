d5c40deffc30af1c9374a49b064fe618cef46184
gabrielpriss <gabrielpriss@hotmail.com>
2021-09-08 21:38:05 -0300

    arrumando newmovie

--- src/App.js
+++ src/App.js
@@ -1,23 +1,18 @@
 import React from 'react';
-import { BrowserRouter as Router, Switch, Route, BrowserRouter } from 'react-router-dom';
-import './App.css';
-import { EditMovie, MovieList, MovieDetails, NewMovie, NotFound } from './pages';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import { MovieList, MovieDetails, NewMovie, EditMovie, NotFound } from './pages';
 
 function App() {
   return (
     <BrowserRouter>
-      <Router>
-        <div>
-          Movie Card Library CRUD
-          <Switch>
-            <Route exact path="/" component={ MovieList } />
-            <Route exact path="/movies/new" component={ NewMovie } />
-            <Route exact path="/movies/:id" render={ () => <MovieDetails /> } />
-            <Route exact path="/movies/:id/edit" component={ EditMovie } />
-            <Route exact path={ undefined } component={ NotFound } />
-          </Switch>
-        </div>
-      </Router>
+      <h1>Movie Card Library CRUD</h1>
+      <Switch>
+        <Route exact path="/" component={ MovieList } />
+        <Route exact path="/movies/new" component={ NewMovie } />
+        <Route exact path="/movies/:id" component={ MovieDetails } />
+        <Route exact path="/movies/:id/edit" component={ EditMovie } />
+        <Route exact path="*" component={ NotFound } />
+      </Switch>
     </BrowserRouter>
   );
 }
--- src/components/MovieCard.js
+++ src/components/MovieCard.js
@@ -1,20 +1,17 @@
 import React from 'react';
-import PropTypes from 'prop-types';
 import { Link } from 'react-router-dom';
+import PropTypes from 'prop-types';
 
 class MovieCard extends React.Component {
   render() {
     const { movie } = this.props;
-    const { title, subtitle, storyline, imagePath, id } = movie;
+
     return (
-      <div className="movie-card" data-testid="movie-card">
-        <img alt="Movie Cover" className="movie-card-image" src={ imagePath } />
-        <div className="movie-card-body">
-          <h4 data-testid="movie-card-title" className="movie-card-title">{title}</h4>
-          <h5 className="movie-card-subtitle">{subtitle}</h5>
-          <p className="movie-card-storyline">{storyline}</p>
-          <Link to={ `/movies/${id}` }>VER DETALHES</Link>
-        </div>
+      <div data-testid="movie-card">
+        <h2>{ movie.title }</h2>
+        <img src={ movie.imagePath } alt={ movie.title } />
+        <h3>{ movie.storyline }</h3>
+        <Link to={ `/movies/${movie.id}` }>VER DETALHES</Link>
       </div>
     );
   }
@@ -22,13 +19,11 @@ class MovieCard extends React.Component {
 
 MovieCard.propTypes = {
   movie: PropTypes.shape({
-    title: PropTypes.string,
-    subtitle: PropTypes.string,
-    storyline: PropTypes.string,
     id: PropTypes.number,
-    rating: PropTypes.oneOfType([PropTypes.string, PropTypes.number]),
+    title: PropTypes.string,
+    storyline: PropTypes.string,
     imagePath: PropTypes.string,
-  }).isRequired,
-};
+  }),
+}.isRequired;
 
 export default MovieCard;
