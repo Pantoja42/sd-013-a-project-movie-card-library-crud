faeb08c3ea7cbb21426e0fa478a40eceab84bdb5
Cristiano Vitorino <57003922+cristianovitorino@users.noreply.github.com>
2021-09-08 23:46:30 -0300

    Adiciona requisito 2 e 3

--- src/pages/MovieList.js
+++ src/pages/MovieList.js
@@ -1,7 +1,10 @@
 import React, { Component } from 'react';
+import { Route } from 'react-router';
+import { Link } from 'react-router-dom';
 import MovieCard from '../components/MovieCard';
 
 import * as movieAPI from '../services/movieAPI';
+import { Loading } from '../components';
 
 class MovieList extends Component {
   constructor() {
@@ -9,17 +12,29 @@ class MovieList extends Component {
 
     this.state = {
       movies: [],
+      load: true,
     };
   }
 
+  componentDidMount() {
+    movieAPI.getMovies().then((data) => {
+      this.setState({
+        movies: data,
+        load: false,
+      });
+    });
+  }
+
   render() {
-    const { movies } = this.state;
+    const { movies, load } = this.state;
 
     // Render Loading here if the request is still happening
 
     return (
       <div data-testid="movie-list">
+        {load && <Loading />}
         {movies.map((movie) => <MovieCard key={ movie.title } movie={ movie } />)}
+        <Link to="/movies/new">ADICIONAR CARTÃO</Link>
       </div>
     );
   }
