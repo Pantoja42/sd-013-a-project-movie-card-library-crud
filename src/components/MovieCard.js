76bf96966c6f9d3881ab9e2ed6e93e651b261b5b
Yuri <yurifranco18@live.com>
2021-09-09 08:53:31 -0300

    foi feito req. 4 e 7

--- src/components/MovieCard.js
+++ src/components/MovieCard.js
@@ -1,13 +1,32 @@
 import React from 'react';
+import PropTypes from 'prop-types';
+import { Link } from 'react-router-dom';
 
 class MovieCard extends React.Component {
   render() {
+    const { movie:
+      {
+        id,
+        imagePath,
+        title,
+        storyline,
+      },
+    } = this.props;
     return (
       <div data-testid="movie-card">
-        Movie Card
+        <img src={ imagePath } alt={ title } />
+        <h1>{ title }</h1>
+        <h4>{ storyline }</h4>
+        <Link to={ `/movies/${id}` }>VER DETALHES</Link>
       </div>
     );
   }
 }
+
+MovieCard.propTypes = {
+  movie: PropTypes.shape({
+    title: PropTypes.string,
+  }),
+}.isRequired;
 
 export default MovieCard;
--- src/pages/MovieDetails.js
+++ src/pages/MovieDetails.js
@@ -1,25 +1,76 @@
 import React, { Component } from 'react';
-
+import PropTypes from 'prop-types';
+import { Link } from 'react-router-dom';
 import * as movieAPI from '../services/movieAPI';
 import { Loading } from '../components';
 
 class MovieDetails extends Component {
-  render() {
-    // Change the condition to check the state
-    // if (true) return <Loading />;
+  constructor(props) {
+    super(props);
+    this.state = {
+      loading: true,
+      filme: {},
+    };
+    this.resolveGetMovie = this.resolveGetMovie.bind(this);
+    this.btnDelete = this.btnDelete.bind(this);
+  }
 
-    const { title, storyline, imagePath, genre, rating, subtitle } = {};
+  componentDidMount() {
+    this.resolveGetMovie();
+  }
 
+  buildMovieCard = (filme) => {
+    const {
+      title,
+      storyline,
+      imagePath,
+      genre,
+      rating,
+      subtitle,
+      id,
+    } = filme;
     return (
       <div data-testid="movie-details">
         <img alt="Movie Cover" src={ `../${imagePath}` } />
+        <h1>{ title }</h1>
         <p>{ `Subtitle: ${subtitle}` }</p>
         <p>{ `Storyline: ${storyline}` }</p>
         <p>{ `Genre: ${genre}` }</p>
         <p>{ `Rating: ${rating}` }</p>
+        <Link to="/">VOLTAR</Link>
+        <Link to={ `/movies/${id}/edit` }>EDITAR</Link>
+        <Link to="/" onClick={ () => this.btnDelete(id) }>DELETAR</Link>
       </div>
     );
   }
+
+  async btnDelete(id) {
+    await movieAPI.deleteMovie(id);
+  }
+
+  async resolveGetMovie() {
+    const { match: { params: { id } } } = this.props;
+    const filme = await movieAPI.getMovie(id);
+    this.setState({
+      loading: false,
+      filme,
+    });
+  }
+
+  render() {
+    const { loading, filme } = this.state;
+    return (
+      loading ? <Loading /> : this.buildMovieCard(filme)
+    );
+  }
 }
+
+MovieDetails.propTypes = {
+  match: PropTypes.shape({
+    params: PropTypes.shape({
+      id: PropTypes.string,
+    }),
+  }).isRequired,
+};
 
 export default MovieDetails;
